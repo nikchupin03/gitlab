842ace93246da3689e99858a6acffc9245ab245d
Randy Rautha <randyrau@student.21-school.ru>
2023-05-30 12:23:57 +0700

    initial

---
+++ gitlab.sh
@@ -0,0 +1,9 @@
+sudo docker run --detach \
+  --publish 8880:443 --publish 8881:80 --publish 8882:22 \
+  --name gitlab \
+  --restart always \
+  --volume $GITLAB_HOME/config:/etc/gitlab \
+  --volume $GITLAB_HOME/logs:/var/log/gitlab \
+  --volume $GITLAB_HOME/data:/var/opt/gitlab \
+  --shm-size 256m \
+  gitlab/gitlab-ee:latest
